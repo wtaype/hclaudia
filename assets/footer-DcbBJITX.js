f9ee6aa97cf459b47f8244e3893b4cefe22c978e
wtaype <wtaype@users.noreply.github.com>
2025-11-21 01:29:29 +0000

    deploy: 8fee8e984601151da13f80d09d2347f4054e9c18

---
+++ assets/footer-DcbBJITX.js
@@ -0,0 +1,8 @@
+import{$ as o}from"./widev-B94L0ewu.js";let e="Registros HClaudia",r=2024,n="@wilder.taype",s="https://wtaype.github.io/",t="v15";o("title").text(`${e} - ${t}`);function i(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${s}" target="_blank">${n}</a></span>
+    <span>${r} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | Acerca del app ${t} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}o("style").append(".foo{width:100%;text-align:center;padding-block:1.5vh 1vh;background:var(--wb);border-radius:1vh 1vh 0 0;}.foo *{font-size:var(--fz_s2);margin-inline:.3vh;}.foo a{color:var(--bg2);}.foo i{color:var(--mco);}.abwc{background:var(--bg);top:0;width:99%;height:100%;padding:2vh 2vw;overflow:scroll;line-height:1.80;}.abwok{background:var(--mco);color:var(--txa);}");export{i as f};
